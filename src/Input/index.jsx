64890881fdbcd5684374bdb2cc6d81943f7d21f1
Sultan Suleev <42939663+sultansuleev@users.noreply.github.com>
2021-04-07 22:24:12 +0600

    Update Input Component

--- src/Input/index.jsx
+++ src/Input/index.jsx
@@ -10,7 +10,6 @@ export default class InputVal extends Component {
                   <input type="number" name = "uppest" placeholder="Введите наибольший отрезок"/>
                   <input type="number" name = "fragments" placeholder="Введите количество фрагментов"/>
                   <button>Вычислить</button>
-                  <h1>Suleev Enterprise & Co.</h1>
               </form>
 
           </Container>
